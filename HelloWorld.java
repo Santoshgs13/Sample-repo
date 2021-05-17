b156c48ac922e6af241839608c3be610dd0cbb42
santosh <santoshguttal47@gmail.com>
2021-05-17 09:20:16 +0530

    Resolving conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,9 +5,10 @@ public class HelloWorld {
 		system.out.println("\n Hello, ");
 		System.out.println("\n Welcome to Treosoftit ");
 		System.out.println("\n Welcome to Waycool ");
+		System.out.println("\n Happy to see you");
 		int num1 = 5, num2 = 15, res,res1;
 		res = num2 + num1;
-		res1 = num2 - num1;
+		res1 = num2 - num;
 		System.out.println("Sum of these numbers: " +res);
 		System.out.println("diff of these numbers: " +res1);
 	}
