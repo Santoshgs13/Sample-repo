75db0d6ad35a27a11dbbee754b002174745f776b
santosh <santoshguttal47@gmail.com>
2021-05-17 08:14:03 +0530

    2nd Change from developer2

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,9 @@ public class HelloWorld {
 		System.out.println("\n Hello, ");
 		System.out.println("\n Welcome to Treosoftit ");
 		System.out.println("\n Welcome to Waycool ");
-		System.out.println("\n Congrulations");
+		int num1 = 5, num2 = 15, res;
+		res = num2 + num1;
+		System.out.println("Sum of these numbers: " +res);
 	}
 
 }
