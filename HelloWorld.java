48a773473ad6f5bfdce4b47d919f8043afe8c834
santosh <santoshguttal47@gmail.com>
2021-05-17 08:16:45 +0530

    From developer1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,12 +2,14 @@ public class HelloWorld {
 
 	public static void main(String[] args)
 	{
-		System.out.println("\n Hello, ");
+		system.out.println("\n Hello, ");
 		System.out.println("\n Welcome to Treosoftit ");
 		System.out.println("\n Welcome to Waycool ");
-		int num1 = 5, num2 = 15, res;
+		int num1 = 5, num2 = 15, res,res1;
 		res = num2 + num1;
+		res1 = num2 - num1;
 		System.out.println("Sum of these numbers: " +res);
+		System.out.println("diff of these numbers: " +res1);
 	}
 
 }
